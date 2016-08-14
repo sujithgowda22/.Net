23ebad794efcf7e862e76ab723a439d95584abd0
sujithgowda22 <sujithgowda22@gmail.com>
2016-08-14 13:10:16 +0530

    firstcommit

---
+++ src/app/HelloCI.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloCI.Web.Startup))]
+namespace HelloCI.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
